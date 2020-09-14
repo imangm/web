3ef3025296874263ae82eb217c79c8c50b56a91d
ImanGM <credo61@gmail.com>
2020-09-14 17:21:05 +0430

    oop in js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
-const robot = {
-	_model: "1E78V2",
-	_energyLevel: 100,
-	_numOfSensors: 15,
-	set numOfSensors(num) {
-		if (typeof num === "number" && num >= 0) {
-			this._numOfSensors = num;
-		} else {
-			console.log("Pass in a number that is greater than or equal to 0");
-		}
-	},
-	get numOfSensors() {
-		if (typeof this._numOfSensors === "number") {
-			return this._numOfSensors;
-		} else {
-			return "Sensors are currently down.";
-		}
-	},
-};
+function circle(radius) {
+	this.radius = radius;
 
-robot.numOfSensors = 100;
+	this.name = function (fn) {
+		return this.radius * fn;
+	};
 
-console.log(robot.numOfSensors);
+	this.draw = function () {
+		console.log("circle is drew!");
+		console.log(`It's as big as ${this.name(4)}`);
+	};
+}
+
+const myCircle = new circle(10);
+console.log(myCircle.radius);
+myCircle.draw();
+console.log(myCircle.name(3));
