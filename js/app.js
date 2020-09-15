7187378c000aeb939080734c2649aec2837b8d25
ImanGM <credo61@gmail.com>
2020-09-15 20:19:09 +0430

    some js functions

--- js/app.js
+++ js/app.js
@@ -1,28 +1,66 @@
-/** Stop Watch Exercise */
-function Stopwatch() {
-	let started = false;
-	let duration = 0;
-	let timeStarted,
-		timeStopped = 0;
-	this.start = function () {
-		if (started) throw new Error("It's already started");
-		// start counting
-		timeStarted = new Date();
-		started = true;
-	};
-	this.stop = function () {
-		if (!started) throw new Error("It's not started right now!");
-		timeStopped = new Date();
-		duration = duration + (timeStopped - timeStarted) / 1000;
-		started = false;
-	};
-	this.reset = function () {
-		duration = 0;
-		started = false;
-	};
-	Object.defineProperty(this, "duration", {
-		get: function () {
-			return duration;
-		},
-	});
+const cl = (m) => console.log(m);
+
+let arr = [0, 4, 1, 5, 7, 3];
+let strx = "This is gonna be something to try";
+let words = ["Ryan", "Kieran", "Jason", "Matt"];
+let nullArr = ["a", null, "b", null];
+let numArr = [-3, 4, -9, -1, -2, 15];
+
+function findLargestNum(arr) {
+	return Math.max(...arr);
 }
+
+function firstLast(arr) {
+	return [arr.shift(), arr.pop()];
+}
+
+function countWords(str) {
+	return str.split(" ").length;
+}
+
+function isFourLetters(arr) {
+	return arr.filter((x) => x.length === 4);
+}
+
+function isBetween(first, last, word) {
+	if (word > first && word < last) return true;
+	return false;
+}
+
+function removeNull(arr) {
+	return arr.filter((x) => x != null);
+}
+
+function reverse(bool) {
+	return typeof bool === "boolean" ? !bool : "boolean expected";
+}
+
+function both(n1, n2) {
+	return Math.sign(n1) == Math.sign(n2);
+}
+
+function nameShuffle(str) {
+	return str.split(" ").reverse().join(" ");
+}
+
+function difference(nums) {
+	return nums.sort((a, b) => b - a).shift() - nums.sort((a, b) => b - a).pop();
+}
+
+function isSafeBridge(str) {
+	return str.split(" ").length === 1;
+}
+
+function negate(arr) {
+	return arr.map((x) => -+x);
+}
+
+function multiplyByLength(arr) {
+	return arr.map((x) => x * arr.length);
+}
+
+function findSmallestNum(arr) {
+	return Math.min(...arr);
+}
+
+cl(findSmallestNum(numArr));
