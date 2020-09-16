318553e237df892e38ff77d8d210159db1bb7fc7
ImanGM <credo61@gmail.com>
2020-09-16 17:42:55 +0430

    more advanced

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ class Dog extends Animal {
 
 const cl = (m) => console.log(m);
 
-let arr = [0, 4, 1, 5, 7, 3];
+let arr = [0, 4, 1, 5, 7, 3, 8];
 let strx = "This is gonna be something to try";
 let words = ["Ryan", "Kieran", "Jason", "Matt"];
 let nullArr = ["a", null, "b", null];
@@ -79,10 +79,140 @@ function findSmallestNum(arr) {
 	return Math.min(...arr);
 }
 
+function noOdds(arr) {
+	return arr.filter((x) => x % 2);
+}
+
+//cl(noOdds(arr));
+
 let Menu = {};
 Menu.special = "Have you ever tried English foods?";
-cl(Menu.special);
+// cl(Menu.special);
 
 module.exports = Menu;
 
-cl(findSmallestNum(numArr));
+// cl(findSmallestNum(numArr));
+
+function displaySpeedRangeStatus() {
+	availableAirplanes.forEach((element) => {
+		console.log(
+			element.name +
+				"meets speed range requirements: " +
+				meetSpeedRangeRequirements(
+					element.maxSpeed,
+					element.minSpeed,
+					flightRequirements.requiredSpeedRange
+				)
+		);
+	});
+}
+
+// cl(20 + 10 + "30");
+
+const sumX = (x) => (y) => (z) => x + y + z;
+
+// cl(sumX(3, 5, 7));
+// cl(sumX(3)(5)(7));
+
+// cl("lets save the world".split("").reverse().join(""));
+
+let prizes = ["one", "two", "three"];
+for (let btnNum = 0; btnNum < prizes.length; btnNum++) {
+	// cl(prizes[btnNum]);
+}
+
+class BasicPlan {
+	static canStream = true;
+	static canDownload = true;
+	static numOfDevices = 1;
+	static hasSD = true;
+	static hasHD = false;
+	static hasUHD = false;
+	static price = "$8.99";
+}
+
+// Write the classes for StandardPlan and PremiumPlan here!
+class StandardPlan extends BasicPlan {
+	static hasHD = true;
+	static numOfDevices = 2;
+	static price = "$12.99";
+}
+
+// cl("BP SD: " + BasicPlan.hasSD); // ➞ true
+// cl("BP UHD: " + BasicPlan.hasUHD); // ➞ false
+// cl("BP Price: " + BasicPlan.price); // ➞ '$8.99'
+// cl("ST HD: " + StandardPlan.hasHD);
+// cl("ST Price: " + StandardPlan.price);
+// cl("ST Num: " + StandardPlan.numOfDevices);
+
+// class PremiumPlan extends BasicPlan {
+// 	constructor() {
+// 		super();
+// 		hasHD = true;
+// 		hasUHD = true;
+// 		numOfDevices = 4;
+// 		price = "$15.99";
+// 	}
+// }
+
+function monthName(num) {
+	return new Date(`2020/${num}/01`).toLocaleString("default", {
+		month: "long",
+	});
+}
+
+//cl(monthName(4));
+
+function monthNameNew(num) {
+	const monthsStr = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	return monthsStr[num - 1];
+}
+//cl(monthNameNew(4));
+
+// This is crazy :D
+arr = [2, 4, 6, 8, 3, 1, 5, 7, 9, 11, 12, 13, 14];
+function oddeven(arr) {
+	return arr.reduce((acc, val) => acc + (val % 2 ? 1 : -1), 0) > 0;
+}
+//cl(oddeven(arr));
+
+function Go(num) {
+	console.log("-".repeat(num));
+}
+//Go(5);
+
+function hurdleJump(hurdles, jumpHeight) {
+	return Math.max(...hurdles) <= jumpHeight;
+}
+// cl(hurdleJump([5, 5, 3, 4, 5], 3));
+
+function AlphabetSoup(str) {
+	return str.split("").sort().join("");
+}
+// cl(AlphabetSoup("hello"));
+
+function reverseCapitalize(str) {
+	return str.toUpperCase().split("").reverse().join("");
+}
+// cl(reverseCapitalize("hellothere"));
+
+function reverse(str) {
+	return [...str].reverse().join("");
+}
+// cl(reverse("Edabit is really helpful!"));
+
+function repetition(txt, n) {}
+repetition("cherry", 2);
